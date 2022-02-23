d35e9ec713d6b35585b54b4e84dff74c0f849c6b
Mohammadafaz <afaz.munshi49@gmail.com>
2022-02-23 17:30:38 -0600

    Created a method to add two numbers instead of doing it from main method

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,17 @@
 public class Calculator
 {
+
+  public static int add(int a, int b)
+  {
+    System.out.println("Adding the numbers...");
+    return a + b;
+  }
+
   public static void main(String [] args)
   {
     int a = 8;
     int b = 9;
-    int c = a + b;
-    System.out.println(c);
+    int c = add(a, b);
+    System.out.println("Result: " + c);
   }
 }
